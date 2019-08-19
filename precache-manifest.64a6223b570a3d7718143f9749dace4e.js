b5e3c6cf24cdd0568b1b94c876fa4c4d6f77ce47
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2019-08-19 23:52:29 +0200

    Updates

---
+++ precache-manifest.64a6223b570a3d7718143f9749dace4e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c9ddca60d7ec2c4187c398b7254e377",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55da885b4f704e73f073",
+    "url": "/static/css/main.3da3ba43.chunk.css"
+  },
+  {
+    "revision": "b6db9005dd8ed66b3996",
+    "url": "/static/js/2.2a2f9352.chunk.js"
+  },
+  {
+    "revision": "55da885b4f704e73f073",
+    "url": "/static/js/main.bb9f5198.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
